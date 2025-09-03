60f36a818ef1638715d5466e74bd8e718885f8be
Ben Sivoravong <bensivo@gmail.com>
2025-09-02 19:46:35 -0500

    Added some more jotai patterns

--- webapp/src/components/Counter.tsx
+++ webapp/src/components/Counter.tsx
@@ -1,14 +1,19 @@
 import { useAtom } from 'jotai';
-import { counterAtom } from '../store/atoms';
+import { counterAtom, counterTimesTwoAtom, doubleAtom } from '../store/atoms';
 
 export const Counter = () => {
   const [count, setCount] = useAtom(counterAtom);
+  const [counterTimesTwo] = useAtom(counterTimesTwoAtom);
+  const [_, double] = useAtom(doubleAtom);
+
 
   return (
     <div>
       <p>{count}</p>
+      <p>Count * 2 = {counterTimesTwo}</p>
       <button onClick={() => setCount(c => c - 1)}>-</button>
       <button onClick={() => setCount(c => c + 1)}>+</button>
+      <button onClick={() => double()}>Double</button>
     </div>
   );
 };
--- webapp/src/store/atoms.ts
+++ webapp/src/store/atoms.ts
@@ -2,3 +2,15 @@ import { atom } from 'jotai';
 
 // Simple counter atom
 export const counterAtom = atom(0);
+
+// Derived atom
+export const counterTimesTwoAtom = atom((get) => {
+    const counter = get(counterAtom);
+    return counter * 2
+});
+
+// Action atom - doesn't hold any values itself, but has a 'setter', which actually sets other atoms
+export const doubleAtom = atom(null, (get, set) => {
+    const counter = get(counterAtom);
+    set(counterAtom, counter * 2)
+})
