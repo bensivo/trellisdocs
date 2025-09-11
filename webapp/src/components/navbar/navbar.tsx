9e03ce87a28b290e7f9dcadd351b5700f924e113
Tate, Thane <thanetate@my.unt.edu>
2025-09-11 10:36:55 -0500

    Feature - Navbar is responsive

--- webapp/src/components/navbar/navbar.tsx
+++ webapp/src/components/navbar/navbar.tsx
@@ -8,18 +8,18 @@ export function NavbarComponent() {
 	return (
 		<div className="navbar-container">
 			<div className="icons-container">
-				<i className="ri-search-line"></i>
+				<i className="ri-search-line icon"></i>
 				<i
-					className="ri-folders-line"
+					className="ri-folders-line icon"
 					style={{ cursor: "pointer" }}
 					onClick={() => navigate("/")}
 				></i>
 				<i
-					className="ri-file-copy-2-line"
+					className="ri-file-copy-2-line icon"
 					style={{ cursor: "pointer" }}
 					onClick={() => navigate("/template")}
 				></i>
-				<i className="ri-shapes-line"></i>
+				<i className="ri-shapes-line icon"></i>
 			</div>
 		</div>
 	);
