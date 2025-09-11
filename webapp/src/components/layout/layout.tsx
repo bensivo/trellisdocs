ab8da3b7e6209d0d49e03fb9ce3fda45864ecdfe
Tate, Thane <thanetate@my.unt.edu>
2025-09-11 10:28:43 -0500

    Fix - responsive layout component

--- webapp/src/components/layout/layout.tsx
+++ webapp/src/components/layout/layout.tsx
@@ -1,9 +1,12 @@
+import { NavbarComponent } from "../navbar/navbar";
 import "./layout.less";
 
 export function LayoutComponent() {
 	return (
 		<div className="layout-lr-container">
-			<div className="layout-left"></div>
+			<div className="layout-left">
+				<NavbarComponent/>
+			</div>
 			<div className="layout-right"></div>
 		</div>
 	);
