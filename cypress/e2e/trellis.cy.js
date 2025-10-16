9fef8fb0a845dfe952646678d6a0a63694a0e88b
Tate, Thane <thanetate@my.unt.edu>
2025-10-16 12:57:43 -0500

    Init - Tests for each page

--- cypress/e2e/trellis.cy.js
+++ cypress/e2e/trellis.cy.js
@@ -1,6 +1,41 @@
-// visit homepage
-describe('Visit Homepage', () => {
+// visit home page
+describe('Visit Home Page', () => {
   it('passes', () => {
     cy.visit('http://localhost:5173/')
+  })
+})
+
+// visit login page 
+describe('Visit Login Page', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5173/login')
+  })
+})
+
+// visit search page
+describe('Visit Search Page', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5173/search')
+  })
+})
+
+// visit integration page
+describe('Visit Integrations Page', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5173/integrations')
+  })
+})
+
+// visit template browser page
+describe('Visit Template Browser Page', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5173/template')
+  })
+})
+
+// visit template editor page
+describe('Visit Template Editor Page', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5173/template/new')
   })
 })
