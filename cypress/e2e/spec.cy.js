a2b841f3ae282ccfc10a74b0b5f786b2d10c6c4f
Tate, Thane <thanetate@my.unt.edu>
2025-10-16 12:45:23 -0500

    Init - Tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,20 @@
+// examples of basic e2e tests
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+  })
+});
+
+// passing test example
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
+
+// failing test example
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(false)
   })
 })
---
+++ cypress/e2e/trellis.cy.js
@@ -0,0 +1,6 @@
+// visit homepage
+describe('Visit Homepage', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5173/')
+  })
+})
